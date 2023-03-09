d441538a13533dc1d9c265612447ad7b1be500fc
GBFerro <giovani.ferro@usp.br>
2023-03-09 11:15:07 -0300

    Perfumando codigo para melhor visualizacao

--- SomaCaminhosMatrizes/Program.cs
+++ SomaCaminhosMatrizes/Program.cs
@@ -6,35 +6,35 @@ double[] vetorAux4 = new double[1];
 
 Random rnd = new Random();
 
-for (int i = 0; i < 5; i++)
+for (int row = 0; row < 5; row++)
 {
-    for (int j = 0; j < 5; j++)
+    for (int col = 0; col < 5; col++)
     {
-        int num = rnd.Next(100);
-        table[i, j] = num;
-        Console.Write(table[i, j] + " ");
+        int num = rnd.Next(10);
+        table[row, col] = num;
+        Console.Write(" | " + table[row, col] + " | ");
     }
     Console.WriteLine("\n");
 }
 
-for (int i = 0; i < 5; i++)
+for (int row = 0; row < 5; row++)
 {
-    for (int j = 0; j < 5; j++)
+    for (int col = 0; col < 5; col++)
     {
-        vetorAux[i] += table[i, j];
+        vetorAux[row] += table[row, col];
     }
-    Console.WriteLine(vetorAux[i] + " ");
+    Console.WriteLine($"Soma da linha {row + 1}: {vetorAux[row]} ");
 }
 
 Console.WriteLine("\n");
 
-for (int i = 0; i < 5; i++)
+for (int col = 0; col < 5; col++)
 {
-    for (int j = 0; j < 5; j++)
+    for (int row = 0; row < 5; row++)
     {
-        vetorAux2[i] += table[j, i];
+        vetorAux2[col] += table[row, col];
     }
-    Console.Write(vetorAux2[i] + " ");
+    Console.WriteLine($"Soma da coluna {col + 1}: {vetorAux2[col]} ");
 }
 
 Console.WriteLine("\n");
@@ -44,5 +44,5 @@ for (int i = 0;i < 5; i++)
     vetorAux3[0] += table[i, i];
     vetorAux4[0] += table[4 - i, i];
 }
-Console.Write(vetorAux3[0] + " ");
-Console.Write(vetorAux4[0] + " ");
+Console.WriteLine($"Soma da diagonal principal: {vetorAux3[0]} ");
+Console.WriteLine($"Soma da diagonal principal: {vetorAux4[0]} ");
