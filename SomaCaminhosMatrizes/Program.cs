1308b3599f9968b3dab4a0e98f492932784e0727
GBFerro <giovani.ferro@usp.br>
2023-03-08 23:30:36 -0300

    Add project files.

---
+++ SomaCaminhosMatrizes/Program.cs
@@ -0,0 +1,48 @@
+﻿double[,] table = new double[5, 5];
+double[] vetorAux = new double[5];
+double[] vetorAux2 = new double[5];
+double[] vetorAux3 = new double[1];
+double[] vetorAux4 = new double[1];
+
+Random rnd = new Random();
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        int num = rnd.Next(100);
+        table[i, j] = num;
+        Console.Write(table[i, j] + " ");
+    }
+    Console.WriteLine("\n");
+}
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        vetorAux[i] += table[i, j];
+    }
+    Console.WriteLine(vetorAux[i] + " ");
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        vetorAux2[i] += table[j, i];
+    }
+    Console.Write(vetorAux2[i] + " ");
+}
+
+Console.WriteLine("\n");
+
+for (int i = 0;i < 5; i++)
+{
+    vetorAux3[0] += table[i, i];
+    vetorAux4[0] += table[4 - i, i];
+}
+Console.Write(vetorAux3[0] + " ");
+Console.Write(vetorAux4[0] + " ");
